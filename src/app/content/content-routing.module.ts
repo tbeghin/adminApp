187d5793c214561431f01fcb2dde8787db406244
Thomas BEGHIN <thomas.beghin@orange.fr>
2017-07-18 18:53:25 +0200

    feat(route) : routage

--- src/app/content/content-routing.module.ts
+++ src/app/content/content-routing.module.ts
@@ -8,7 +8,7 @@ const appRoutes: Routes = [
   {
     path: '', component: ContentComponent,
     children: [
-      {path: '', component: DashboardComponent}
+      {path: 'dashboard', component: DashboardComponent}
     ]
   }
 ];

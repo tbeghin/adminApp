dfaba2de162306df625c4f5c032c653927ebaad7
Thomas BEGHIN <thomas.beghin.micropole@axa.fr>
2017-10-13 14:07:27 +0200

    feat (forgetPassword) : Ajout de la fonction mot de passe oublié

--- src/assets/main-admin.js
+++ src/assets/main-admin.js
@@ -98,10 +98,4 @@ $(function () {
 
   //Activate bootstrip tooltips
   $("[data-toggle='tooltip']").tooltip();
-
-  // Login Page Flipbox control
-  $('.login-content [data-toggle="flip"]').click(function() {
-    $('.login-box').toggleClass('flipped');
-    return false;
-  });
 });
